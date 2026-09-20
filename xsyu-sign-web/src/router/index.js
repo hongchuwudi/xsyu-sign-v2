bf5db916bb2d3a0473b1e06be2d75796cc7247be
hongchuwudi <2772167017@qq.com>
2026-09-20 18:37:51 +0800

    fix: use hash routing for page refreshes

--- xsyu-sign-web/src/router/index.js
+++ xsyu-sign-web/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 
 // 页面按 公共 / 用户 / 管理员 三大类组织，模块陆续迁移中，路由逐步接入
@@ -71,7 +71,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
